53e6b812a5e7fd23529892b4b006ca1a2a231e10
gjan <gerhard.jansen@web.de>
2019-05-26 23:22:23 +0200

    implement UI of register page

---
+++ front-end/src/views/RegisterPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="register-form">
+        <div class="logo-wrapper">
+          <img class="logo" src="/static/images/logo.png" />
+          <span class="tagline">Open source task management tool</span>
+        </div>
+        <form>
+          <div class="form-group">
+            <input id="username" type="text"/>
+            </div>
+          <div class="form-group">
+            <input id="emailAddress" type="text"/>
+
+          </div>
+          <div class="form-group">
+            <input id="password" type="text"/>
+
+          </div>
+          <button type="submit" class="btn btn-primary btn-block">Create account</button>
+          ...
+        </form>
+      </div>
+    </div>
+    <footer class="footer">
+      <span class="copyright">...</span>
+      <ul class="footer-links list-inline float-right">...</ul>
+    </footer>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 900px;
+}
+.register-form {
+  margin-top: 50px;
+  max-width: 320px;
+}
+.logo-wrapper {
+  margin-bottom: 40px;
+}
+.footer {
+  width: 100%;
+  line-height: 40px;
+  margin-top: 50px;
+}
+</style>
