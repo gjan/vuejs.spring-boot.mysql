b1e32f9102cff2ff540e98ee9d8851a05d636127
gdevelop2017 <gdevelop@ibb-d.de>
2019-05-26 11:41:15 +0200

    create backend scaffold

---
+++ src/main/java/de/gdevelop/taskagile/app/AppApplication.java
@@ -0,0 +1,13 @@
+package de.gdevelop.taskagile.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAgileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskAgileApplication.class, args);
+	}
+
+}
